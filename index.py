075bb7740e3331eb6dbd31568e3113b2df7cbe87
Felipe Greg <mrfelipesensei@gmail.com>
2025-03-11 15:17:47 -0300

    gerar_senha_interface()

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 import random
 import string #Contém letras e símbolos úteis
+import tkinter as tk
+from tkinter import messagebox
+
 
 #Função de gerar a senha
 def gerar_senha(tamanho=12, incluir_numeros=True, incluir_maiusculas=True, incluir_simbolos=True):
@@ -18,7 +21,7 @@ def gerar_senha(tamanho=12, incluir_numeros=True, incluir_maiusculas=True, inclu
 '''#Teste básico
 print(gerar_senha(16)) #Gera uma senha de 16 caracteres'''
 
-def main():
+def modo_terminal():
     tamanho = int(input("Digite o tamanho da senha: "))
     quantidade = int(input("Quantas senhas deseja gerar? "))
     incluir_numeros = input("Incluir números? (s/n): ").strip().lower() == 's'
@@ -31,7 +34,27 @@ def main():
     for senha in senhas:
         print(senha)
     
-    return senhas
+    salvar_senhas(senhas)
+
+def gerar_senha_interface():
+    try:
+        tamanho = int(entry_tamanho.get())
+        quantidade = int(entry_quantidade.get())
+        incluir_numeros = var_numeros.get()
+        incluir_maiusculas = var_maiusculas.get()
+        incluir_simbolos = var_simbolos.get()
+
+        senhas = [gerar_senha(tamanho, incluir_numeros, incluir_maiusculas, incluir_simbolos) for _ in range(quantidade)]
+        resultado.delete("1.0", tk.END)
+        resultado.insert(tk.END, "\n".join(senhas))
+
+        salvar_senhas(senhas)
+        messagebox.showinfo("Sucesso","Senhas geradas e salvas em 'senhas.txt'")
+
+    except ValueError:
+        messagebox.showerror("Erro", "Digite valores numéricos válidos!")
+
+
 
 import os
 
@@ -47,5 +70,15 @@ def salvar_senhas(senhas):
 
 
 if __name__ == "__main__":
-    senhas = main()
-    salvar_senhas(senhas)
+    print("Escolha o modo de execução: ")
+    print("1 - Modo Terminal")
+    print("2 - Interface Gráfica")
+
+    opcao = input("Digite o número da opção desejada: ").strip()
+
+    if opcao == "1":
+        modo_terminal()
+    elif opcao == "2":
+        iniciar_interface()
+    else:
+        print("Opção Inválida!")
